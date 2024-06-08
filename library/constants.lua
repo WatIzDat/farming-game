62faa6f4db16c794cf6b82f4cbcac1d8a906c204
WatIzDat <lily3575@hotmail.com>
2024-06-08 16:36:14 -0700

    Implement fertility based on distance from water

--- library/constants.lua
+++ library/constants.lua
@@ -4,7 +4,9 @@ M.tiles = {}
 
 M.tiles.WATER_TILE = 1
 M.tiles.LAND_TILE = 2
-M.tiles.FARMLAND_TILE = 3
+M.tiles.UNFERTILE_FARMLAND_TILE = 3
+M.tiles.MIDFERTILE_FARMLAND_TILE = 4
+M.tiles.FERTILE_FARMLAND_TILE = 5
 
 M.layers = {}
 
