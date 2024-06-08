3276f14b20287cd1533f54ffa5331781db28d659
WatIzDat <lily3575@hotmail.com>
2024-06-08 15:35:14 -0700

    Refactor using constants instead of magic strings

---
+++ library/constants.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.tiles = {}
+
+M.tiles.WATER_TILE = 1
+M.tiles.LAND_TILE = 2
+M.tiles.FARMLAND_TILE = 3
+
+M.layers = {}
+
+M.layers.GROUND = "ground"
+
+M.input = {}
+
+M.input.NEW_WORLD = hash("new_world")
+M.input.CAMERA_PAN = hash("camera_pan")
+M.input.CAMERA_ZOOM_IN = hash("camera_zoom_in")
+M.input.CAMERA_ZOOM_OUT = hash("camera_zoom_out")
+M.input.TILL_LAND = hash("till_land")
+
+return M
