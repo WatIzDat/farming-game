fdb25d29699e4be3a3c5e088786f420013f844a8
WatIzDat <lily3575@hotmail.com>
2024-06-09 16:49:06 -0700

    Implement basic crop harvesting functionality
    Add inventory
    Rename world_gen to world

--- library/constants.lua
+++ library/constants.lua
@@ -22,6 +22,10 @@ M.input.CAMERA_PAN = hash("camera_pan")
 M.input.CAMERA_ZOOM_IN = hash("camera_zoom_in")
 M.input.CAMERA_ZOOM_OUT = hash("camera_zoom_out")
 M.input.TILL_LAND = hash("till_land")
-M.input.PLANT_CROP = hash("plant_crop")
+M.input.PLANT_OR_DESTROY_CROP = hash("plant_or_destroy_crop")
+
+M.messages = {}
+
+M.messages.HARVEST_CROP = hash("harvest_crop")
 
 return M
